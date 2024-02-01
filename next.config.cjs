36c664e955d776c77c72ec73d4f5ce643b27732e
Dhravya <dhravyashah@gmail.com>
2024-01-31 19:34:00 -0700

    aded tracking url

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,7 @@
 const withPWA = require('@ducanh2912/next-pwa').default({
-    dest: 'public',
-    register: true,
-    disable: process.env.NODE_ENV === 'development',
-  });
+  dest: 'public',
+  register: true,
+});
 
 await import("./src/env.js");
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,19 @@ import '@/styles/globals.css';
 
 import { Inter } from 'next/font/google';
 import Providers from './providers';
-import { type Metadata } from 'next';
+import { Viewport, type Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-sans',
 });
 
-const title =
-  'Notty - a simple, minimal, AI powered note taking app and markdown editor';
+const title = 'Notty';
 const description =
   'Notty is a simple, minimal AI powered note taking app and markdown editor - Built local-first, with cloud sync. It uses AI to help you write and stay productive.';
 
 export const metadata: Metadata = {
+  applicationName: 'Notty',
   title,
   description,
   openGraph: {
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
         url: '/ogimage.png',
         alt: title,
       },
-    ]
+    ],
   },
   twitter: {
     title,
@@ -35,6 +35,16 @@ export const metadata: Metadata = {
   },
   metadataBase: new URL('https://notty.dhr.wtf/'),
   icons: ['/logo.png'],
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#54CFDF',
 };
 
 export default function RootLayout({
@@ -44,6 +54,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          defer
+          data-website-id="cbf1706f-88f5-4acc-90fc-31a0724c50eb"
+          src="https://u.dhr.wtf/umami.js"
+        ></script>
+      </head>
       <body className={`font-sans ${inter.variable}`}>
         <Providers>
           <main>{children}</main>

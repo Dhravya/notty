c7eaf5097edb7524246c4c411ee64ae6a0f39cdb
Dhravya <dhravyashah@gmail.com>
2024-02-05 15:52:10 -0700

    update pwa

--- next.config.cjs
+++ next.config.cjs
@@ -2,10 +2,9 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
 });
 
-await import("./src/env.js");
+const nextConfig = {};
 
-const config = withPWA({});
-
-export default config;  
+module.exports = withPWA(nextConfig);

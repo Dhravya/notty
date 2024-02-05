fc12a539b11c9491eca2269fa78efb268906ff4b
Dhravya <dhravyashah@gmail.com>
2024-02-05 16:03:54 -0700

    embedchain service worker

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,8 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
+const withPWA = require('next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
+  //   disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {};

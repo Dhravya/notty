9dbd0f982c3ed496aee861f9d057fd92c10efb8f
Dhravya <dhravyashah@gmail.com>
2024-02-07 23:50:07 -0700

    fix: build

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { notFound, useSearchParams } from "next/navigation";
+import { notFound } from "next/navigation";
 import NovelEditor from "./[id]/novelEditor";
 
-function Page() {
+function Page({ searchParams }: { searchParams: { id: string } }) {
 
-    const params = useSearchParams();
-
-    const id = params.get("id");
-
-    if (!id) return notFound()
+    if (!searchParams.id){
+        notFound()
+    }
 
     return (
         <div className="mb-12 flex min-h-[100svh] flex-col items-center sm:px-5 sm:pt-[calc(20vh)] md:mb-0">
-            <NovelEditor id={id} />
+            <NovelEditor id={searchParams.id} />
         </div>
     );
 }

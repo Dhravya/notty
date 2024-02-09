59f1135d2b2c8f2bce8aaff32d62cdc3af3cad35
Dhravya <dhravyashah@gmail.com>
2024-02-08 19:07:09 -0700

    use openai instead of openrouter

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -7,7 +7,7 @@ import { env } from "@/env";
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: env.OPENROUTER_API_TOKEN,
-  baseURL: "https://openrouter.ai/api/v1/",
+  // baseURL: "https://openrouter.ai/api/v1/",
 });
 
 // IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
